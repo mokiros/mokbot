b7dcac5c2b36de0206c9fdf9723017ef138da5a9
mokiros <mokiros@yandex.ru>
2023-11-12 19:52:02 +0300

    Removed unused bot token from production code

--- src/Env.d.ts
+++ src/Env.d.ts
@@ -1,7 +1,6 @@
 export interface Env {
 	DISCORD_APP_ID: string
 	DISCORD_APP_PUBLIC_KEY: string
-	DISCORD_BOT_TOKEN: string
 	ROBLOX_BADGE_LIMIT: string
 	CACHE_NODE_EXPIRATION_SECONDS: string
 
--- src/lib/discord/fetchDiscordApi.ts
+++ src/lib/discord/fetchDiscordApi.ts
@@ -11,7 +11,6 @@ export default async function fetchDiscordApi<T extends object>(
 		method,
 		headers: {
 			'Content-Type': 'application/json',
-			'Authorization': `Bot ${env.DISCORD_BOT_TOKEN}`,
 		},
 		body: body ? JSON.stringify(body) : undefined,
 	})
